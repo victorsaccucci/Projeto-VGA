91161b25cea81025a77a8e31ca303a799619e431
ggc12037 <ggc12037@p50082.intralesc.sc.gov.br>
2023-05-19 15:19:27 -0300

    Criado a classe ItemVO, ProdutoVO, VendaVO, UsuarioVO

--- src/main/java/model/vo/UsuarioVO.java
+++ src/main/java/model/vo/UsuarioVO.java
@@ -2,4 +2,77 @@ package model.vo;
 
 public class UsuarioVO {
 
+	private int id;
+	private String nome;
+	private String senha;
+	private String email;
+	private String cpf;
+	
+	
+	public UsuarioVO() {
+		super();
+	}
+	
+	public UsuarioVO(String nome, String senha, String email, String cpf) {
+		super();
+		this.nome = nome;
+		this.senha = senha;
+		this.email = email;
+		this.cpf = cpf;
+	}
+	
+	public UsuarioVO(int id, String nome, String senha, String email, String cpf) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.senha = senha;
+		this.email = email;
+		this.cpf = cpf;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public String toString() {
+		return  id + " " + nome + " " + senha + " " + email + " " + cpf;
+	}
+	
 }
---
+++ src/main/java/model/vo/VendaVO.java
@@ -0,0 +1,66 @@
+package model.vo;
+
+public class VendaVO {
+
+	private int id;
+	private int idItem;
+	private int idUsuario;
+	private String dataVenda;
+	
+	public VendaVO() {
+		super();
+	}
+
+	public VendaVO(int idItem) {
+		super();
+		this.idItem = idItem;
+	}
+
+	public VendaVO(int id, int idItem, int idUsuario, String dataVenda) {
+		super();
+		this.id = id;
+		this.idItem = idItem;
+		this.idUsuario = idUsuario;
+		this.dataVenda = dataVenda;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getIdItem() {
+		return idItem;
+	}
+
+	public void setIdItem(int idItem) {
+		this.idItem = idItem;
+	}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getDataVenda() {
+		return dataVenda;
+	}
+
+	public void setDataVenda(String dataVenda) {
+		this.dataVenda = dataVenda;
+	}
+
+	@Override
+	public String toString() {
+		return id + " " + idItem + " " + idUsuario + " " + dataVenda;
+	}
+	
+	
+	
+}
