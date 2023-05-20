0477b4e5a98afbf22c1501ee7e7f0ab405c9244e
arthu <arthu@ARTHUR-PC>
2023-05-20 12:46:31 -0300

    Criado novos métodos em ProdutoDAO
    Métodos criados:
     atualizar
     excluir

--- src/main/java/model/dao/ProdutoDAO.java
+++ src/main/java/model/dao/ProdutoDAO.java
@@ -13,24 +13,74 @@ public class ProdutoDAO {
 		
 		Connection conn = Banco.getConnection();
 		String sql = "INSERT INTO PRODUTO (DESCRICAO, MARCA) VALUES (?,?)";
-		PreparedStatement stmt = Banco.getPreparedStatementWithPk(conn, sql);
+		PreparedStatement query = Banco.getPreparedStatementWithPk(conn, sql);
 		try {
-			stmt.setString(1,novoProduto.getDescricao());
-			stmt.setString(2, novoProduto.getMarca());
-			stmt.execute();
+			query.setString(1,novoProduto.getDescricao());
+			query.setString(2, novoProduto.getMarca());
+			query.execute();
 			
-			ResultSet resultado = stmt.getGeneratedKeys();
+			ResultSet resultado = query.getGeneratedKeys();
 			if(resultado.next()) {
 				novoProduto.setId(resultado.getInt(1));
 			}
 		} catch(SQLException e){
-			System.out.println("Erro ao inserir um novo produto!" + e.getMessage());
+			System.out.println("Erro ao inserir um novo produto!" 
+								+ "\n Causa: " + e.getMessage());
 		} finally {
+			Banco.closePreparedStatement(query);
 			Banco.closeConnection(conn);
-			Banco.closePreparedStatement(stmt);
 		}
 		
 		return novoProduto;
+	}
+	
+	public boolean excluir(int id) {
+		boolean excluiu = false;
+		
+		Connection conexao = Banco.getConnection();
+		String sql = " DELETE FROM PRODUTO WHERE ID = ? ";
+		PreparedStatement query = Banco.getPreparedStatement(conexao, sql);
+		try {
+			query.setInt(1, id);
+			
+			int linhasAtualizadas = query.executeUpdate();
+			excluiu = linhasAtualizadas > 0;
+		} catch (SQLException e) {
+			System.out.println("Erro ao excluir produto! " 
+							+ "\n Causa: " + e.getMessage());
+		} finally {
+			Banco.closePreparedStatement(query);
+			Banco.closeConnection(conexao);
+		}
+				
+		return excluiu;
+	}
+	
+	public boolean atualizar(ProdutoVO produtoEditado) {
+		boolean atualizou = false;
+		
+		Connection conexao = Banco.getConnection();
+		String sql = " UPDATE PRODUTO "
+				   + " SET DESCRICAO = ?, MARCA = ? "
+				   + " WHERE ID = ? ";
+		PreparedStatement query = Banco.getPreparedStatement(conexao, sql);
+		try {
+			query.setString(1, produtoEditado.getDescricao());
+			query.setString(2, produtoEditado.getMarca());
+			query.setInt(3, produtoEditado.getId());
+			
+			int linhasAtualizadas = query.executeUpdate();
+			atualizou = linhasAtualizadas > 0;
+			
+		} catch (SQLException e) {
+			System.out.println("Erro ao atualizar produto! "
+								+ "\n Causa: " + e.getMessage());
+		} finally {
+			Banco.closePreparedStatement(query);
+			Banco.closeConnection(conexao);
+		}
+		
+		return atualizou;
 	}
 
 }
