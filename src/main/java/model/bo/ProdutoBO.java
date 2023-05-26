d7e034b75819e7479657c60f94dea513b186b0b1
arthur.germano <arthur.germano@FLE207D31.alunos.sc.senac.br>
2023-05-26 09:24:10 -0300

    Atualização em ProdutoBO

--- src/main/java/model/bo/ProdutoBO.java
+++ src/main/java/model/bo/ProdutoBO.java
@@ -1,5 +1,16 @@
 package model.bo;
 
+import model.ExceptionVGA;
+import model.dao.ProdutoDAO;
+import model.vo.ProdutoVO;
+
 public class ProdutoBO {
+	
+	ProdutoDAO dao = new ProdutoDAO();
+	
+	public ProdutoVO inserir(ProdutoVO novoProduto) throws ExceptionVGA{
+		
+		return dao.inserir(novoProduto);
+	}
 
 }
