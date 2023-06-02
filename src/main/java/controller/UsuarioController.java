b352c342469d0b7a3a32cc8eb37a242ddaa6b7ed
arthur.germano <arthur.germano@FLE104D13.alunos.sc.senac.br>
2023-06-02 09:29:02 -0300

    Adicionado novos métodos em UsuarioController

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.util.ArrayList;
 
+import model.ExceptionVGA;
 import model.bo.UsuarioBO;
 import model.vo.UsuarioVO;
 
@@ -12,8 +13,9 @@ public class UsuarioController {
 		return usuarioBO.realizarLoginBO(usuarioVO);
 
 	}	
-	public UsuarioVO cadastrarUsuarioController(UsuarioVO usuarioVO) {
+	public UsuarioVO cadastrarUsuarioController(UsuarioVO usuarioVO) throws ExceptionVGA {
 		UsuarioBO usuarioBO = new UsuarioBO();
+		this.validarCamposObrigatorios(usuarioVO);
 		return usuarioBO.cadastrarUsuarioBO(usuarioVO);
 	}
 	public ArrayList<UsuarioVO> consultarTodosUsuariosController() {
@@ -33,5 +35,43 @@ public class UsuarioController {
 	public boolean excluirUsuarioController(UsuarioVO usuarioVO) {
 		UsuarioBO usuarioBO = new UsuarioBO();
 		return usuarioBO.excluirUsuarioBO(usuarioVO);
+	}
+	
+	private void validarCamposObrigatorios(UsuarioVO usuario) throws ExceptionVGA {
+		String validacao = "";
+		
+		if(usuario.getNome() == null || usuario.getNome().trim().length() < 2) {
+			validacao += "Nome inválido \n";
+		}
+		
+		validacao += validarCpf(usuario);
+		
+		if(usuario.getEmail() == null) {
+			validacao += "Informe um endereço \n";
+		}
+		
+		if(usuario.getSenha() == null) {
+			validacao += "Informe uma senha! \n";
+		}
+		
+		if(!validacao.isEmpty()) {
+			throw new ExceptionVGA(validacao);
+		}
+	}
+	
+	private String validarCpf(UsuarioVO usuario) throws ExceptionVGA{
+		String validacao = "";
+		
+		if(usuario.getCpf() == null) {
+			validacao += "Informe um CPF! \n";
+		}else {
+			String cpfSemMascara = usuario.getCpf().replace(".", "");
+			cpfSemMascara = usuario.getCpf().replace("-", "");
+			usuario.setCpf(cpfSemMascara);
+			if(usuario.getCpf().length() != 11) {
+				validacao += "CPF deve possuir 11 digitos! \n";
+			}
+		}
+		return validacao;
 	}
 }
