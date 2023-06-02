7c4e16d4a84f0a215b7bc27037730934fd3bd3f0
arthur.germano <arthur.germano@FLE104D13.alunos.sc.senac.br>
2023-06-02 09:28:12 -0300

    Atualizado ProdutoBO

--- src/main/java/model/bo/ProdutoBO.java
+++ src/main/java/model/bo/ProdutoBO.java
@@ -1,5 +1,7 @@
 package model.bo;
 
+import java.util.List;
+
 import model.ExceptionVGA;
 import model.dao.ProdutoDAO;
 import model.vo.ProdutoVO;
@@ -11,6 +13,22 @@ public class ProdutoBO {
 	public ProdutoVO inserir(ProdutoVO novoProduto) throws ExceptionVGA{
 		
 		return dao.inserir(novoProduto);
+	}
+	
+	public boolean atualizar(ProdutoVO produtoAlterado) {
+		return dao.atualizar(produtoAlterado);
+	}
+	
+	public boolean excluir(int id) {
+		return dao.excluir(id);
+	}
+	
+	public ProdutoVO consultarPorId(int id) {
+		return dao.consultarPorId(id);
+	}
+	
+	public List<ProdutoVO> consultarTodos(){
+		return dao.consultarTodos();
 	}
 
 }
