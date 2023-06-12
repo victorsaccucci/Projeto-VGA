39e2cc8a9bbe20167dc694cfe2429463a28648c2
arthu <arthu@ARTHUR-PC>
2023-06-12 18:52:37 -0300

    Adicionado botões funcionais

--- src/main/java/view/TelaCarrinho.java
+++ src/main/java/view/TelaCarrinho.java
@@ -1,11 +1,14 @@
 package view;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -14,6 +17,13 @@ public class TelaCarrinho {
 
 	private JFrame frame;
 	private TelaDeProdutos telaDeProdutos;
+	private JLabel lblSair;
+	private JLabel lblMaximizar;
+	private JLabel lblMinimizar;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -73,7 +83,55 @@ public class TelaCarrinho {
 					.addContainerGap(545, Short.MAX_VALUE))
 		);
 		frame.getContentPane().setLayout(groupLayout);
+		
+		lblSair = new JLabel(" X");
+		lblSair.setVerticalAlignment(SwingConstants.TOP);
+		lblSair.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		lblSair.setForeground(Color.WHITE);
+		lblSair.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		lblSair.setBounds(924, 0, 22, 21);
+		frame.getContentPane().add(lblSair);
+		
+		lblMaximizar = new JLabel("  ");
+		lblMaximizar.setBounds(881, 534, 25, 24);
+		frame.getContentPane().add(lblMaximizar);
+
+		lblMinimizar = new JLabel("  ");
+		lblMinimizar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				 frame.setExtendedState(JFrame.ICONIFIED);
+			}
+		});
+		lblMinimizar.setForeground(new Color(255, 255, 255));
+		lblMinimizar.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		lblMinimizar.setBackground(new Color(255, 255, 255));
+		lblMinimizar.setBounds(700, 522, 8, 20);
+		frame.getContentPane().add(lblMinimizar);
+
+		lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(TelaDeProdutos.class.getResource("/icones/icons8-maximizar-10.png")));
+		lblNewLabel_1.setBounds(896, 6, 15, 15);
+		frame.getContentPane().add(lblNewLabel_1);
+
+		lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				 frame.setExtendedState(JFrame.ICONIFIED);
+			}
+		});
+		lblNewLabel_2.setIcon(new ImageIcon(TelaDeProdutos.class.getResource("/icones/icons8-minimizar-15.png")));
+		lblNewLabel_2.setBounds(867, 0, 16, 27);
+		frame.getContentPane().add(lblNewLabel_2);
 	}
+	
+	
 
 	public void tornarVisivelForaDoFrame() {
 		frame.setVisible(true);
