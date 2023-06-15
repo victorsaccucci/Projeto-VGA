48f113def539f242c2b90d0caaa661d2d8ef843d
arthu <arthu@ARTHUR-PC>
2023-06-15 16:45:26 -0300

    Implementado todos filtros para item funcionando.

--- src/main/java/view/PainelListagemItens.java
+++ src/main/java/view/PainelListagemItens.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import controller.ItemController;
 import model.seletor.SeletorItem;
 import model.vo.ItemVO;
@@ -20,10 +23,9 @@ import javax.swing.JTextField;
 
 public class PainelListagemItens extends JPanel {
 
-	
 	private static final long serialVersionUID = -826234024317345122L;
-	
-	public String[] nomeColunas = {"Tamanho", "Cor", "Qtd", "Preço"};
+
+	public String[] nomeColunas = { "Tamanho", "Cor", "Qtd", "Preço" };
 	private JTable tabelaItens;
 	private List<ProdutoVO> produto;
 	private List<ItemVO> item;
@@ -40,77 +42,77 @@ public class PainelListagemItens extends JPanel {
 	private JButton btnBuscarTodos;
 
 	private SeletorItem seletor;
-	
-	
+
 	/**
 	 * Create the panel.
 	 */
-	
+
 	private void limparTabela() {
 		tabelaItens.setModel(new DefaultTableModel(new Object[][] { nomeColunas, }, nomeColunas));
 	}
-	
+
 	private void atualizarTabelaItens() {
 		this.limparTabela();
 
 		DefaultTableModel model = (DefaultTableModel) tabelaItens.getModel();
-		
-			for( ItemVO vo : item) {
-				Object[] novaLinhaDaTabela = new Object[4];
-				novaLinhaDaTabela[0] = vo.getTamanho();
-				novaLinhaDaTabela[1] = vo.getCor();
-				novaLinhaDaTabela[2] = vo.getQuantidade();
-				novaLinhaDaTabela[3] = vo.getPrecoUnitario();
-				model.addRow(novaLinhaDaTabela);
-			}
+
+		for (ItemVO vo : item) {
+			Object[] novaLinhaDaTabela = new Object[4];
+			novaLinhaDaTabela[0] = vo.getTamanho();
+			novaLinhaDaTabela[1] = vo.getCor();
+			novaLinhaDaTabela[2] = vo.getQuantidade();
+			novaLinhaDaTabela[3] = vo.getPrecoUnitario();
+			model.addRow(novaLinhaDaTabela);
+		}
 	}
-	
+
 	public PainelListagemItens() {
 		setBackground(new Color(0, 139, 139));
 		setForeground(new Color(0, 139, 139));
 		setLayout(null);
-		
+
 		lblMenorPreco = new JLabel("Menor Preço:");
 		lblMenorPreco.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		lblMenorPreco.setBounds(57, 61, 89, 26);
 		add(lblMenorPreco);
-		
+
 		lblMaiorPreco = new JLabel("Maior Preço:");
 		lblMaiorPreco.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		lblMaiorPreco.setBounds(57, 132, 92, 26);
 		add(lblMaiorPreco);
-		
+
 		lblTamanho = new JLabel("Tamanho:");
 		lblTamanho.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		lblTamanho.setBounds(366, 61, 67, 26);
 		add(lblTamanho);
-		
+
 		lblCor = new JLabel("Cor:");
 		lblCor.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		lblCor.setBounds(390, 132, 43, 26);
 		add(lblCor);
-		
+
 		txtMenorPreco = new JTextField();
 		txtMenorPreco.setBounds(147, 61, 99, 31);
 		add(txtMenorPreco);
 		txtMenorPreco.setColumns(10);
-		
+
 		txtMaiorPreco = new JTextField();
 		txtMaiorPreco.setColumns(10);
 		txtMaiorPreco.setBounds(147, 132, 99, 31);
 		add(txtMaiorPreco);
-		
+
 		txtTamanho = new JTextField();
 		txtTamanho.setColumns(10);
 		txtTamanho.setBounds(443, 61, 55, 31);
 		add(txtTamanho);
-		
+
 		txtCor = new JTextField();
 		txtCor.setColumns(10);
 		txtCor.setBounds(443, 132, 165, 31);
 		add(txtCor);
-		
+
 		btnBuscar = new JButton("Buscar");
+		btnBuscar.setEnabled(false);
 		btnBuscar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				buscarItensComFiltro();
@@ -119,10 +121,10 @@ public class PainelListagemItens extends JPanel {
 		});
 		btnBuscar.setBounds(648, 132, 89, 31);
 		add(btnBuscar);
-		
+
 		btnBuscarTodos = new JButton("Buscar Todos");
 		btnBuscarTodos.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
 				itemController = new ItemController();
 				item = itemController.consultarTodos();
@@ -131,24 +133,57 @@ public class PainelListagemItens extends JPanel {
 		});
 		btnBuscarTodos.setBounds(759, 132, 128, 31);
 		this.add(btnBuscarTodos);
-		
+
 		tabelaItens = new JTable();
 		tabelaItens.setBounds(57, 193, 830, 340);
 		add(tabelaItens);
-		
+
 		JLabel lblAte = new JLabel("Até");
 		lblAte.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		lblAte.setBounds(57, 98, 36, 26);
 		add(lblAte);
 		
+        txtMaiorPreco.getDocument().addDocumentListener(new MyDocumentListener());
+        txtMenorPreco.getDocument().addDocumentListener(new MyDocumentListener());
+        txtTamanho.getDocument().addDocumentListener(new MyDocumentListener());
+
 	}
+	
+	private class MyDocumentListener implements DocumentListener {
+        public void insertUpdate(DocumentEvent e) {
+            checarCampos();
+        }
+
+        public void removeUpdate(DocumentEvent e) {
+            checarCampos();
+        }
+
+        public void changedUpdate(DocumentEvent e) {
+            checarCampos();
+        }
+    }
+
+	private void checarCampos() {
+		if (!txtMenorPreco.getText().isEmpty() && !txtMaiorPreco.getText().isEmpty() && !txtTamanho.getText().isEmpty()) {
+			btnBuscar.setEnabled(true);
+		} else {
+			btnBuscar.setEnabled(false);
+		}
+	}
+
 	protected void buscarItensComFiltro() {
 		seletor = new SeletorItem();
+		seletor.setCor(txtCor.getText());
+
+		int tamanho = Integer.parseInt(txtTamanho.getText());
+		seletor.setTamanho(tamanho);
+
 		seletor.setPrecoInicial(txtMenorPreco.getText());
 		seletor.setPrecoFinal(txtMaiorPreco.getText());
 		ItemController itemController2 = new ItemController();
 		item = (List<ItemVO>) itemController2.consultarComFiltros(seletor);
 		atualizarTabelaItens();
-		
+
 	}
+
 }
