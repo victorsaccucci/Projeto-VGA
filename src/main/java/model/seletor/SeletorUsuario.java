232d137f0a5dd9b6d16c03029beaf91649777132
Victor Saccucci <victorsaccucci@hotmail.com>
2023-06-16 15:31:32 -0300

    Implementada a classe seletor de usuários

---
+++ src/main/java/model/seletor/SeletorUsuario.java
@@ -0,0 +1,39 @@
+package model.seletor;
+
+public class SeletorUsuario extends SeletorBase{
+	
+	private String email;
+	private String nome;
+	private String cpf;
+	
+	@Override
+	public boolean temFiltro() {
+		return  this.email != null
+				|| this.nome != null
+				|| this.cpf != null;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+}
