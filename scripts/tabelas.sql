6c7efd482da0fb2e2e5ffdca0db384d36db3a8a8
Victor Saccucci <victorsaccucci@hotmail.com>
2023-06-22 10:09:07 -0300

    Script atualizado

--- scripts/tabelas.sql
+++ scripts/tabelas.sql
@@ -1,41 +1,35 @@
+SET SQL_SAFE_UPDATES = 0;
+drop schema projetoVga;
+create schema projetoVga;
+use projetoVga;
+
 CREATE TABLE `item` (
   `iditem` int NOT NULL AUTO_INCREMENT,
   `tamanho` char(2) NOT NULL,
   `cor` varchar(45) NOT NULL,
   `quantidade` int NOT NULL,
-  `preco_unitario` double NOT NULL,
+  `precoUnitario` double NOT NULL,
   `idproduto` int NOT NULL,
+  `imagem` blob,
   PRIMARY KEY (`iditem`),
   KEY `idproduto_idx` (`idproduto`),
   CONSTRAINT `idproduto` FOREIGN KEY (`idproduto`) REFERENCES `produto` (`idproduto`)
-) 
-
+);
 
 CREATE TABLE `produto` (
   `idproduto` int NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(45) NOT NULL,
+  `modelo` varchar(45) NOT NULL,
   `marca` varchar(45) NOT NULL,
   PRIMARY KEY (`idproduto`)
-)
+);
 
 
 CREATE TABLE `usuario` (
-  `idusuario` int NOT NULL AUTO_INCREMENT,
+  `idusuario` Integer NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) NOT NULL,
   `senha` varchar(45) NOT NULL,
-  `email` int NOT NULL,
+  `email` varchar(255) NOT NULL,
   `cpf` varchar(11) NOT NULL,
+  `adm` boolean,
   PRIMARY KEY (`idusuario`)
-) 
-
-CREATE TABLE `venda` (
-  `idvenda` int NOT NULL,
-  `data_venda` varchar(45) NOT NULL,
-  `iditem` int NOT NULL,
-  `idusuario` int NOT NULL,
-  PRIMARY KEY (`idvenda`),
-  KEY `iditem_idx` (`iditem`),
-  KEY `idusuario_idx` (`idusuario`),
-  CONSTRAINT `iditem` FOREIGN KEY (`iditem`) REFERENCES `item` (`iditem`),
-  CONSTRAINT `idusuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`)
 );
