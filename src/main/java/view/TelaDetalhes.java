1e670eef13b40e051f731d01946b2f30b3d70096
Victor Saccucci <victorsaccucci@hotmail.com>
2023-06-25 18:10:52 -0300

    Ajuste na janela

--- src/main/java/view/TelaDetalhes.java
+++ src/main/java/view/TelaDetalhes.java
@@ -115,8 +115,6 @@ public class TelaDetalhes {
 		lblVoltar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				telaMenu = new TelaMenuPrincipal();
-				telaMenu.tornarVisivelForaDoFrame();
 				frame.setVisible(false);
 			}
 		});
